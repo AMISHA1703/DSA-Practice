3bbd176493ccf71bf589adff64dfa23528f238d1
Amisha Singh <amishakumari1703@gmail.com>
2025-01-01 15:04:34 +0530

    Update TWO_SUM.cpp

--- Array/TWO_SUM.cpp
+++ Array/TWO_SUM.cpp
@@ -1,23 +1,40 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+ vector<int> twoSum(vector<int>& nums, int target)
+   {
+     vector<int> result;
+       for(int i=0;i<nums.size();i++)
+       {
+           int j=i+1;
+           while(j<nums.size())
+           {
+               if(nums[j]+nums[i]==target)
+               {
+                result.push_back(i);
+                result.push_back(j);
+
+                   return result;
+               }
+               j++;
+           }
+       }
+       return result;
+ } 
 int main()
 {
-    int target;
+    int target=8;
    vector<int> v;
    v.push_back(1);
    v.push_back(2);
    v.push_back(3);
    v.push_back(4);
+    vector<int> ans=twoSum(v,target);
+    cout<<ans<<endl;
 
-     int i=0;int j=v.size()-1;
-       for(int k=0;k<=j;k++)
-       {
-           if(v[i]+v[j]==target) cout<<i<<","<<j;
-           else if(v[i]+v[j]>target) j--;
-           else i++;
-       }
 
 
     return 0;
 }
+
+
